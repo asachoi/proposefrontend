c0176613da4f17762100c4e4f3d305457890175d
Asa HP Choi <Asa HP Choi@fp.manulife.hk>
2016-12-30 12:06:07 +0800

    new file:   images/manulife-logo.png

--- customizations/translate.js
+++ customizations/translate.js
@@ -18,7 +18,9 @@ mainApp.config(
             faceamount: 'Face Amount',
             error_surname: 'Your surname must be between 10 and 100 characters long and look like an e-mail address.',
             premium: 'Basic Annual Premium',
-            riderform: 'Riders'
+            riderform: 'Riders',
+            insurer: 'Insurer',
+            policyholder: 'Policy Holder'
         });
         $translateProvider.translations('de', {
             TITLE: 'Hallo',

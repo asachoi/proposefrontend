e7d1d7001703b57a4c421f043f2fade370a66a4d
Asa HP Choi <Asa HP Choi@fp.manulife.hk>
2017-02-06 16:28:28 +0800

    new file:   scripts/app.module.js 	modified:   scripts/app.routes.js

--- scripts/app.init.js
+++ scripts/app.init.js
@@ -1,118 +1,19 @@
+(function () {
+  'use strict';
 
- 
-var eProposeApp = angular.module('eProposeApp', [
-  'ngMaterial',
-  
-  'ui.router',
-  'pascalprecht.translate',
-  //'ionic',
-  //'ngCordova',
-  //'ngResource',
-  //'tmh.dynamicLocale',  
-  //'slickCarousel',
-  //'LocalStorageModule'
-]);
+  angular.module('eProposeApp', [
+    'ngMaterial',
+    'ui.router',
+    'pascalprecht.translate',
 
-angular.module('eProposeApp').config(function ($mdIconProvider, $mdThemingProvider) {
+  ]);
+
+  angular.module('eProposeApp').config(function ($mdIconProvider, $mdThemingProvider) {
     $mdThemingProvider.theme('default')
-        .primaryPalette('green')
-        .accentPalette('orange');
+      .primaryPalette('green')
+      .accentPalette('orange');
 
     $mdIconProvider.fontSet('md', 'material-icons');
-});
+  });
+})();
 
-
-/*
-eProposeApp
-	.run(function (logging) {
-		logging.init('main');
-		logging.setLogLevel(log4javascript.Level.ALL);
-		logging.setLogAppender(new log4javascript.BrowserConsoleAppender());
-	});
-*/
-
-angular.module('eProposeApp')
-  //define constants
-  .constant('availableLanguages', ['en-US', 'ja-JP', 'en-PH'])
-  .constant('defaultLanguage', 'en-PH');
-/*
-  .run(function ($ionicPlatform, tmhDynamicLocale, $translate, $cordovaGlobalization, $rootScope, $state, availableLanguages, defaultLanguage, localStorageService) {
-    function applyLanguage(language) {
-      tmhDynamicLocale.set(language.toLowerCase());
-    }
-
-    function getSuitableLanguage(language) {
-      for (var index = 0; index < availableLanguages.length; index++) {
-        if (availableLanguages[index].toLowerCase() === language.toLocaleLowerCase()) {
-          return availableLanguages[index];
-        }
-      }
-      return defaultLanguage;
-    }
-
-    function setLanguage() {
-      if (typeof navigator.globalization !== 'undefined') {
-        $cordovaGlobalization.getPreferredLanguage().then(function (result) {
-          var language = getSuitableLanguage(result.value);
-          applyLanguage(language);
-          $translate.use(language);
-          localStorageService.set("localeCode", language.substring(0, 2));
-          localStorageService.set("language", language);
-        });
-      } else {
-        applyLanguage(defaultLanguage);
-        localStorageService.set("localeCode", defaultLanguage.substring(0, 2));
-        localStorageService.set("language", defaultLanguage);
-      }
-    }
-
-    $ionicPlatform.ready(function () {
-
-      setLanguage();
-
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        //Hide the keyboard accessory bar with the next, previous and done buttons.
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-        //Ionic by default uses the Ionic scroll, which simulates the Momentum Scrolling 
-        //as seen on the iOS devices, by adding the overflow-scroll=true attribute, 
-        //you are basically forcing this particular ion-content to use the native scrolling
-        //Note : Prevent the native UIScrollView from moving when an input is focused
-        cordova.plugins.Keyboard.disableScroll(true);
-
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-
-    // add possible global event handlers here
-
-  })
-  .config(['slickCarouselConfig', function (slickCarouselConfig) {
-    slickCarouselConfig.dots = true;
-    slickCarouselConfig.autoplay = false;
-  }])
-  .config(function ($ionicConfigProvider) {
-    // Remove back button text completely
-    $ionicConfigProvider.backButton.previousTitleText(false).text('');
-    $ionicConfigProvider.views.swipeBackEnabled(false);
-  })
-
-  .config(function ($compileProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|blob):|data:image\//);
-  })
-
-  //check connection on every request
-  .config(function ($httpProvider) {
-    $httpProvider.interceptors.push('reqInterceptor');
-    //$httpProvider.interceptors.push('reqErrorInterceptor');
-  })
-  .config(function ($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$')]);
-});
-
-
-
-
-*/
--- scripts/app.routes.js
+++ scripts/app.routes.js
@@ -1,49 +1,55 @@
-angular.module('eProposeApp').config(function ($stateProvider, $urlRouterProvider) {
+(function () {
+    'use strict';
 
-    $urlRouterProvider.otherwise('/list');
+    angular
+        .module('eProposeApp')
+        .config(function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
-        .state('list', {
-            url: '/list',
-            cache: false,
-            templateUrl: 'scripts/views/en/list.html?cb=' + cachebuster
-            ,
-            controller: function ($scope, $rootScope) {
-                var vm = $scope;
-                vm.customerList = $rootScope.customerList;
-            }
-        })
-        .state('form', {
-            url: '/form',
-            cache: false,
-            templateUrl: 'scripts/views/en/form.html?cb=' + cachebuster,
-            controller: 'formController'
-        })
-        .state('form.customer', {
-            url: '/customers',
-            cache: false,
-            templateUrl: 'scripts/views/customer/en/customer.html?cb=' + cachebuster,
-            controller: 'form.customer.controller'
-        })
-        .state('form.baseplan', {
-            url: '/baseplan',
-            cache: false,
-            templateUrl: 'scripts/views/baseplan/en/baseplan.html?cb=' + cachebuster,
-            controller: 'formbaseplanController'
-        })
-        .state('form.rider', {
-            url: '/rider',
-            cache: false,
-            templateUrl: 'scripts/views/rider/en/rider.html?cb=' + cachebuster,
-            controller: 'formriderController'
-        })
-        .state('form.summary', {
-            url: '/summary',
-            cache: false,
-            templateUrl: 'scripts/views/summary/en/summary.html?cb=' + cachebuster,
-            controller: 'form.summary.controller'
-        })
-        ;
+            $urlRouterProvider.otherwise('/list');
+
+            $stateProvider
+                .state('list', {
+                    url: '/list',
+                    cache: false,
+                    templateUrl: 'scripts/views/en/list.html?cb=' + cachebuster
+                    ,
+                    controller: function ($scope, $rootScope) {
+                        var vm = $scope;
+                        vm.customerList = $rootScope.customerList;
+                    }
+                })
+                .state('form', {
+                    url: '/form',
+                    cache: false,
+                    templateUrl: 'scripts/views/en/form.html?cb=' + cachebuster,
+                    controller: 'formController'
+                })
+                .state('form.customer', {
+                    url: '/customers',
+                    cache: false,
+                    templateUrl: 'scripts/views/customer/en/customer.html?cb=' + cachebuster,
+                    controller: 'form.customer.controller'
+                })
+                .state('form.baseplan', {
+                    url: '/baseplan',
+                    cache: false,
+                    templateUrl: 'scripts/views/baseplan/en/baseplan.html?cb=' + cachebuster,
+                    controller: 'formbaseplanController'
+                })
+                .state('form.rider', {
+                    url: '/rider',
+                    cache: false,
+                    templateUrl: 'scripts/views/rider/en/rider.html?cb=' + cachebuster,
+                    controller: 'formriderController'
+                })
+                .state('form.summary', {
+                    url: '/summary',
+                    cache: false,
+                    templateUrl: 'scripts/views/summary/en/summary.html?cb=' + cachebuster,
+                    controller: 'form.summary.controller'
+                })
+                ;
 
 
-});
+        })
+})();
